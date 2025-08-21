c25299a9f47e7195086ae722ee0c51e7292a227a
gonzalomarcylabschoo <gonzalo@marcylabschool.org>
2025-08-21 16:36:46 -0400

    added models

---
+++ server/server.py
@@ -0,0 +1,18 @@
+# TODO: import flask jsonify request cors os datetime
+# standard imports first
+import os
+from datetime import datetime
+
+# third party imports
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+# TODO: initialize flask app
+
+
+# TODO: define routes
+
+# TODO: run the app
+
+# TODO: import flask and other dependencies
+
